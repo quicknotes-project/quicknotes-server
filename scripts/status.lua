85cad4e4b000cf52c8362f5898cb4343efca2bd8
Axtra <axtra2@gmail.com>
2022-11-14 17:03:24 +0300

    changed status.lua

--- scripts/status.lua
+++ scripts/status.lua
@@ -1,29 +1,4 @@
-function os.capture(cmd, raw)
-    local handle = assert(io.popen(cmd, 'r'))
-    local output = assert(handle:read('*a'))
-    
-    handle:close()
-
-    if raw then
-        return output
-    end
-
-    output = string.gsub(
-        string.gsub(
-            string.gsub(output, '^%s+', ''),
-            '%s+$',
-            ''
-        ),
-        '[\n\r]+',
-        ' '
-    )
-    return output
-end
-
--- local uptime = os.capture("uptime")
 local uptime = io.popen("uptime"):read("a"):match("^%s*(.-)%s*$")
-local temp = os.capture("cat /sys/class/thermal/thermal_zone0/temp") / 1000
-
+local temp = tonumber(io.popen("cat /sys/class/thermal/thermal_zone0/temp"):read("a"):match("^%s*(.-)%s*$")) / 1000
 local status = string.format('%s,  CPU temp: %s\'C', uptime, temp)
-
 ngx.say(status)
