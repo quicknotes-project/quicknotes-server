b8b94eaaad694062f5541beb02dd5ffbc13592db
vancomm <peshekhonov.ip@gmail.com>
2022-11-15 18:18:49 +0300

    Minor tweak

--- scripts/status.lua
+++ scripts/status.lua
@@ -1,4 +1,4 @@
 local uptime = io.popen("uptime"):read("a"):match("^%s*(.-)%s*$")
 local temp = tonumber(io.popen("cat /sys/class/thermal/thermal_zone0/temp"):read("a"):match("^%s*(.-)%s*$")) / 1000
-local status = string.format('%s,  CPU temp: %s\'C', uptime, temp)
+local status = string.format('%s\nCPU temp: %s\'C', uptime, temp)
 ngx.say(status)
