c9c39cb0c3888c9e13ecb5f5fcca4d64008a1e54
Axtra <axtra2@gmail.com>
2022-11-25 17:37:25 +0300

    implemented PUT /note

--- scripts/note.lua
+++ scripts/note.lua
@@ -65,12 +65,31 @@ local function handleGET(db)
 
     ngx.say(cjson.encode(note))
     return ngx.HTTP_OK
-    -- return ngx.HTTP_METHOD_NOT_IMPLEMENTED
 end
 
-local function handlePATCH(db)
+local function handlePUT(db)
+    local noteID = tonumber(ngx.req.get_uri_args().noteID)
+    if not noteID then return ngx.HTTP_BAD_REQUEST end
+
     local uid = us.getUserId(ngx.req.get_headers(), db)
     if not uid then return ngx.HTTP_UNAUTHORIZED end
+
+    local auth = false
+    local err = db:exec(
+        string.format(
+          [[SELECT NoteID
+                FROM Notes
+                WHERE NoteID = "%s" AND UserID = "%s"
+                LIMIT 1]],
+            noteID, uid
+        ),
+        function ()
+            auth = true
+            return 0
+        end
+    )
+    if err ~= sqlite3.OK then return ngx.HTTP_INTERNAL_SERVER_ERROR end
+    if not auth then return ngx.HTTP_UNAUTHORIZED end
 
     ngx.req.read_body()
     local dataJSON = ngx.req.get_body_data()
@@ -79,9 +98,26 @@ local function handlePATCH(db)
     local dataTable = cjson.decode(dataJSON)
     if not dataTable then return ngx.HTTP_BAD_REQUEST end
 
-    -- UPDATE here
+    local sql = [[
+        UPDATE Notes
+        SET ModifiedAt = CURRENT_TIMESTAMP]]
 
-    return ngx.HTTP_METHOD_NOT_IMPLEMENTED
+    if dataTable.title then
+        sql = sql .. ', Title = "' .. dataTable.title .. '"'
+    end
+    if dataTable.content then
+        sql = sql .. ', Content = "' .. dataTable.content .. '"'
+    end
+    if dataTable.tags then
+        return ngx.HTTP_METHOD_NOT_IMPLEMENTED
+    end
+
+    sql = sql .. [[ WHERE NoteID = ]] .. noteID
+
+    local err = db:exec(sql)
+    if err ~= sqlite3.OK then return ngx.HTTP_INTERNAL_SERVER_ERROR end
+
+    return ngx.HTTP_OK
 end
 
 local function handleDELETE(db)
@@ -92,12 +128,13 @@ local method = ngx.req.get_method()
 local status = ngx.HTTP_METHOD_NOT_IMPLEMENTED
 local db = sqlite3.open("./data/db.sqlite3")
 
+
 if method == "POST" then
     status = handlePOST(db)
 elseif method == "GET" then
     status = handleGET(db)
-elseif method == "PATCH" then
-    status = handlePATCH(db)
+elseif method == "PUT" then
+    status = handlePUT(db)
 elseif method == "DELETE" then
     status = handleDELETE(db)
 end
